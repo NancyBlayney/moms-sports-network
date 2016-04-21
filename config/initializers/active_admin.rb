e91e7afa0b1a662f338a8af4a3dae9e6a29f69ba
Nancy Blayney <nblayney@g.cofc.edu>
2016-04-21 16:26:28 -0400

    progress

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Msn App"
+  config.site_title = "Moms Sports Network"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -30,7 +30,7 @@ ActiveAdmin.setup do |config|
   # will namespace routes to /hello_world/*
   #
   # To set no namespace by default, use:
-  #   config.default_namespace = false
+    # config.default_namespace = false
   #
   # Default:
   # config.default_namespace = :admin
